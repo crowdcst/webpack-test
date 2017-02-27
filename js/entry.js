e58bd308f6a163407b35ef03429b26cfabee5836
Dylan Jhaveri <dylanjhaveri@gmail.com>
2017-02-27 12:31:11 -0800

    add c.js for dependencies in code splitting

--- js/entry.js
+++ js/entry.js
@@ -1,4 +1,6 @@
 require('./a')
-require.ensure([], function (require) {
+console.log('i am entry outside')
+require.ensure(['./c'], function (require) {
   require('./b')
+  console.log('i am entry inside')
 }, 'videojs')
